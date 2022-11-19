61fb48cfdbcb8d5c6a958262701f76be0d2a79f8
mmagna31 <marco.magnarelli@gmail.com>
2022-11-19 07:07:08 +0100

    setting recipe details in progress

--- src/pages/RecipeShortInfo.js
+++ src/pages/RecipeShortInfo.js
@@ -1,17 +1,13 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import styles from "./RecipeShortInfo.module.css";
 
 const RecipeShortInfo = ({ title, image }) => {
   return (
     <Container>
       <Row className="border rounded">
         <Col xs={12} md={6} className="rounded order-md-1 p-0">
-          <img
-            style={{ width: "100%" }}
-            className="rounded"
-            src={image}
-            alt={title}
-          />
+          <img className={styles.imgInfo} src={image} alt={title} />
         </Col>
         <Col xs={12} md={6} className="order-md-0 p-0">
           {/* <GeneralInfo {...data} /> */}Test
