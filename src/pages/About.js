ac4d4f4a0c4cc7cd803d8efd7f35f7eb195cebf9
mmagna31 <marco.magnarelli@gmail.com>
2022-11-26 01:49:22 +0100

    improve

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,40 +1,38 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import Hero from "../components/Hero";
 import logo from "../assets/img/logo_200x200.png";
+import AboutImg from "../assets/img/spaghetti.jpg";
 
 const About = () => {
   return (
     <>
-      <Hero mask={false}>
+      <Hero img={AboutImg} mask={true} maskRgba="rgba(255, 255, 255, 0.2)">
         <Container fluid>
-          <Row>
-            <Col className="text-center p-5">
-              <h3 className="text-uppercase font-custom">
-                If you don't want meat today, it's the best time for
-              </h3>
-              <h1 className="display-4 fw-bold text-uppercase">
-                A vegetarian day
-              </h1>
-            </Col>
-          </Row>
+          <div className="text-center p-5 text-white">
+            <h3 className="font-custom">
+              If you don't want meat today, it's the best time for
+            </h3>
+            <h1 className="display-4 fw-bold text-uppercase">
+              A vegetarian day
+            </h1>
+          </div>
         </Container>
       </Hero>
-      <Container>
-        <Row>
-          <p className="lead mt-5">
-            There are days when you wake up and don't feel like eating meat.
-            Maybe because you exaggerated the day before or simply because today
-            you want to be light. This is the ideal place to find what you can
-            cook with what you have in the fridge while discovering the most
-            delicious vegetarian dishes.
-          </p>
-        </Row>
-        <Row>
-          <Col className="text-center">
-            <img src={logo} className="w-25" alt="A Vegetarian Day" />
-          </Col>
-        </Row>
+      <Container className="text-center ">
+        <p className="lead mt-5">
+          There are days when you wake up and don't feel like eating meat. Maybe
+          because you exaggerated the day before or simply because today you
+          want to be light. This is the ideal place to find what you can cook
+          with what you have in the fridge while discovering the most delicious
+          vegetarian dishes.
+        </p>
+        <img
+          src={logo}
+          style={{ width: "100px" }}
+          alt="A Vegetarian Day"
+          className="m-2"
+        />
       </Container>
     </>
   );
